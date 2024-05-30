381338632e73e5c4c5c2289a1b8ef4035824e3c1
F0sya <nikitachmyria@gmail.com>
2024-05-30 19:26:16 +0300

    First practice exercise is done.

--- cpp_array_part3/main.cpp
+++ cpp_array_part3/main.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+    srand(time(0));
+    const int full_col = 100;
+    const int full_row = 100;
+    float row, col, sum = 0;
+    float average = 0;
+    do
+    {
+        cout << "Enter the number of columns: ";
+        cin >> row;
+    } while (row < 1 || row > 100);
+    do
+    {
+        cout << "Enter the number of row: ";
+        cin >> col;
+    } while (col < 1 || col > 100);
+
+    int arr[full_col][full_row];
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            arr[i][j] = rand() % 10;
+            sum += arr[i][j];
+        }
+    }
+
+    average = sum / (col * row);
+    int min = arr[0][0];
+    int max = arr[0][0];
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            if (arr[i][j] < min) {
+                min = arr[i][j];
+            }
+            if (arr[i][j] > max) {
+                max = arr[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Summary:" << sum << endl
+        << "Average:" << average << endl
+        << "Min:" << min << endl
+        << "Max:" << max << endl;
+
+    return 0;
+
 
 }
